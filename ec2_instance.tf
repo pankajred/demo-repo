839983304cdeff51a2a16e3ef64279c9615d5d69
pankaj bhagwani <bhagwanipankaj13@gmail.com>
2021-06-16 13:41:57 +0530

    Add files via upload

--- ec2_instance.tf
+++ ec2_instance.tf
@@ -1,23 +1,23 @@
-//terraform {
-//  required_providers {
-//    aws = {
-//      source  = "hashicorp/aws"
-//      version = "3.44.0"
-//    }
-//  }
-//}
-//
-//provider "aws" {
-//  region = "ap-southeast-2"
-//}
-//
-//resource "aws_instance" "my_ec2" {
-//  ami                         = "ami-0186908e2fdeea8f3"
-//  instance_type               = "t2.micro"
-//  associate_public_ip_address = true
-//
-//  tags = {
-//    Name = "my_ec2_machine"
-//  }
-//}
-//
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "3.44.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "ap-southeast-2"
+}
+
+resource "aws_instance" "my_ec2" {
+  ami                         = "ami-0186908e2fdeea8f3"
+  instance_type               = "t2.micro"
+  associate_public_ip_address = true
+
+  tags = {
+    Name = "my_ec2_machine"
+  }
+}
+
